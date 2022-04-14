117cbb9cd3d1c9f27766a520335c7653c070c469
REFSZIN <refwire@gmail.com>
2022-04-14 18:00:50 -0300

    1.3.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+const UOLAPI = "https://mock-api.driven.com.br/api/v6/uol";
 let telaLogin = document.querySelector("nav.login");
 let telaAside = document.querySelector("section.aside");
 let input = document.querySelector(".inputmsg");
@@ -11,30 +12,83 @@ let exitAside = document.querySelector("aside .exit");
 let publicoCheck = document.querySelector(".publico ion-icon");
 let privadoCheck = document.querySelector(".reservado ion-icon");
 let userCheck = document.querySelector(".checkon");
-let meuUsuario = {};
-let destinatario = "Todos";
-let ultimaVisibilidade = "Público";
+let meuUsuario = null;
 let participantes = [];
 //-------------------------------------------------------------------------
+function abrirMenu (){
+    telaAside.classList.toggle("ativo")
+};
 function enterLogin (){
     enter = event.keyCode;
     if (enter === 13 && inputLogin.value.length >= 3){
     login();
-    }
-}
+    };
+};
 function login (){
     meuUsuario = inputLogin.value;
-    if (meuUsuario.length >= 3)
-    telaLogin.classList.add("desativo");
-    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', meuUsuario).then(sucessoEntrada).catch(erroUsuario);
-}
-function abrirMenu (){
-    telaAside.classList.toggle("ativo")
-}
-// function sucessoEntrada(){
+    const promise = axios.post(`${UOLAPI}/participants`, { name: meuUsuario});
+    promise.then(function (){
+        telaLogin.classList.add("desativo");
+        sucessoEntrada();
+        setInterval(sucessoEntrada, 100000000000);
+        setInterval(usuarioConectado, 5000);
+    });
+    promise.catch(function (){
+        alert(`Nome: ${meuUsuario} já cadrastado!`)
+    });
+};
+function sucessoEntrada (){
+    const promise= axios.get(`${UOLAPI}/messages`)
+    promise.then(function(mensagens){
+    console.log(mensagens.data);
+    aplicarMsgs(mensagens.data);
+    });
+    promise.catch(function(){
+    })
+};
+function usuarioConectado (){
+    const promise = axios.post(`${UOLAPI}/status`, { name: meuUsuario});
+    promise.then(function (){
+        console.log("Conectado")
+    });
+    promise.catch(function (){
+        alert(`${meuUsuario} voçe foi desconectado!`)
+        window.location.reload();
+    });
+};
+function aplicarMsgs (mensagens){
+    listaMsg.scrollIntoView();
+    mensagens.forEach( mensagem => {
+        const type = mensagem.type;
+        const rementente = mensagem.from;
+        const destino = mensagem.to;
+        const time = mensagem.time;
+        const texto= mensagem.text;
 
-// }
-// function erroUsuario (){
-
-// }
-//-------------------------------------------------------------------
+        if(type === "status") {
+            listaMsg.innerHTML += 
+            `<article class="log">
+            <span class="horario">${time}</span>
+            <h2 class="nome">${rementente}:</h2>
+            <h3 class="msg">${texto}</h3>
+            </article>`
+        };
+        if  (type === "message") {
+            listaMsg.innerHTML +=
+            `<article class="global">
+            <span class="horario">${time}</span>
+            <h2 class="nome">${rementente}:</h2>
+            <h3 class="msg">${texto}</h3>
+            </article>`
+        };
+        if(type === "private_message") {
+            listaMsg.innerHTML +=
+            `<article class="msgprive">
+            <span class="horario">${time}</span>
+            <h2 class="nome">${rementente}:</h2>
+            <h2 class="nomeremetente"> para ${destino}:</h2>
+            <h3 class="msg">${texto}</h3>
+            </article>`
+        };
+    });
+};
